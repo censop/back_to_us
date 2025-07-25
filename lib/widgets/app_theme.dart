815f8e258a23826a9c0c9493bdd3af2aa387f999
Cansu <cansuiris@gmail.com>
2025-07-25 10:37:55 +0300

    static font name

--- lib/widgets/app_theme.dart
+++ lib/widgets/app_theme.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static String fontName = "Poppins";
+
+
+
   static ThemeData myTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(255, 130, 14, 42),
@@ -11,43 +15,43 @@ class AppTheme {
   scaffoldBackgroundColor: Colors.white,
   textTheme: TextTheme(
     displayLarge: TextStyle(
-      fontFamily: "Poppins",
+      fontFamily: fontName,
       fontSize: 48,
       fontWeight: FontWeight.w700,
       color: const Color.fromARGB(255, 130, 14, 42),
     ),
     displayMedium: TextStyle(
-      fontFamily: "Poppins",
+      fontFamily: fontName,
       fontSize: 48,
       fontWeight: FontWeight.w700,
       color: const Color.fromARGB(255, 130, 14, 42),
     ),
     titleLarge: TextStyle(
-      fontFamily: "Poppins",
+      fontFamily: fontName,
       fontSize: 24,
       fontWeight: FontWeight.w600,
       color: Colors.black87,
     ),
     titleMedium: TextStyle(
-      fontFamily: "Poppins",
+      fontFamily: fontName,
       fontSize: 20,
       fontWeight: FontWeight.w600,
       color: Colors.black87,
     ),         
     bodyLarge: TextStyle(
-      fontFamily: "Poppins",
+      fontFamily: fontName,
       fontSize: 16,
       fontWeight: FontWeight.normal,
       color: Colors.black87,
     ),
     bodyMedium: TextStyle(
-      fontFamily: "Poppins",
+      fontFamily: fontName,
       fontSize: 14,
       fontWeight: FontWeight.normal,
       color: Colors.black87,
     ),
     labelLarge: TextStyle(
-      fontFamily: "Poppins",
+      fontFamily: fontName,
       fontSize: 14,
       fontWeight: FontWeight.w500,
       color: const Color.fromARGB(255, 130, 14, 42),
